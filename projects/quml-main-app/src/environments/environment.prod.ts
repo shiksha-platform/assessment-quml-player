121234364eb1d48aaa7a31a74e7afaa05493fa2f
Manoj Singh Rawat <manojrawat1502@gmail.com>
2022-08-23 13:30:17 +0530

    parentURl and prod build

--- projects/quml-main-app/src/environments/environment.prod.ts
+++ projects/quml-main-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     url: 'https://vdn.diksha.gov.in',
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5NGNlMDFjMjU1M2U0MDc2OTVlNzE3ZTU2NTQ1Zjk1NCJ9.wJEBhQuC1cG9CET9RlBGc749h7nzTgMJG1GeOPO-3mc',
-    parentUrl: 'http://127.0.0.1:3009'
+    parentUrl: 'https://sandbox.shikshaplatform.io'
 };
